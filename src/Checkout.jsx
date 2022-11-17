bbfc8fa81f576cd16c6b3353a015eae364547eab
goyal-shobhit <shobhitgoyal46@gmail.com>
2022-11-17 18:13:21 +0530

    eshop

---
+++ src/Checkout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./checkout.css"
+import CheckoutProduct from './CheckoutProduct'
+const Checkout = () => {
+  return (
+    <div className='checkout'>
+        <div className="checkout__left">
+   <img src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" 
+   alt="" className="checkout__ad" />
+   <div>
+    <h2 className="checkout__title">
+        Your Shopping Basket
+    </h2>
+    <CheckoutProduct/>
+   </div>
+        </div>
+    <div className="checkout__right">
+
+    </div>
+    </div>
+  )
+}
+
+export default Checkout
